3152c8e6086840153674efedff708cda218ebebd
L3w15DM <117673970+L3w15DM@users.noreply.github.com>
2023-10-01 23:12:05 -0500

    creat models

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using MusicLibraryAPI.Models;
 
 namespace MusicLibraryAPI.Data
 {
 	public class ApplicationDbContext : DbContext
 	{
+		public DbSet<Song> Songs { get; set; }
+
 		public ApplicationDbContext(DbContextOptions options) : base(options)
 		{
 
---
+++ Models/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MusicLibraryAPI.Models
+{
+	public class Song
+	{
+		public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        public Song()
+		{
+		}
+	}
+}
+
